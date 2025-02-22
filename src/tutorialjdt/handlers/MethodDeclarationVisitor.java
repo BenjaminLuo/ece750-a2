7729456f65952bc78aba18e977f674642742435b
b33luo <b33luo@uwaterloo.ca>
2025-02-22 18:53:00 -0500

    Added generic exceptions, improved getLocation function

--- src/tutorialjdt/handlers/MethodDeclarationVisitor.java
+++ src/tutorialjdt/handlers/MethodDeclarationVisitor.java
@@ -1,5 +1,6 @@
 package tutorialjdt.handlers;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.Type;
@@ -29,12 +30,24 @@ public class MethodDeclarationVisitor extends ASTVisitor {
     
 
     private String getLocation(int startPosition) {
+    	// Output the file, line number, and method declaration text
         try {
+        	String fileNameString = unit.getResource().getLocation().toOSString();
             IDocument document = new Document(unit.getSource());
             int lineNumber = document.getLineOfOffset(startPosition) + 1;
             IRegion lineInfo = document.getLineInformation(lineNumber - 1);
             String lineText = document.get(lineInfo.getOffset(), lineInfo.getLength());
-            return "File: " + unit.getElementName() + ", Line: " + lineNumber + "\n" + lineText;
+            // return "File: " + unit.getElementName() + ", Line: " + lineNumber + "\n" + lineText;
+            
+            // Check if the line starts with a decorator
+            if (lineText.trim().startsWith("@")) {
+                IRegion nextLineInfo = document.getLineInformation(lineNumber);
+                String nextLineText = nextLineInfo != null ? document.get(nextLineInfo.getOffset(), nextLineInfo.getLength()) : "";
+                return "\nFile: " + fileNameString + ", Line: " + lineNumber + "\n" + lineText + "\n" + nextLineText;
+            } else {
+                return "\nFile: " + fileNameString + ", Line: " + lineNumber + "\n" + lineText;
+            }
+            
         } catch (Exception e) {
             e.printStackTrace();
             return "File: " + unit.getElementName() + ", Line: Unknown";
@@ -47,6 +60,7 @@ public class MethodDeclarationVisitor extends ASTVisitor {
 		// Get list of exceptions in the method declaration
 		@SuppressWarnings("unchecked")
 		List<Type> exceptions = node.thrownExceptionTypes();
+		List<String> genericExceptions = Arrays.asList("Exception", "RuntimeException", "IOException", "NullPointerException", "ArrayIndexOutOfBoundsException", "SQLException");
 
 		// Count exceptions
 		int numExceptions = exceptions.size();
@@ -54,7 +68,7 @@ public class MethodDeclarationVisitor extends ASTVisitor {
 		// Throws Generic
         for (Type e : exceptions) {
         	String exceptionTypeString = e.toString();
-            if ("Exception".equals(exceptionTypeString)) {
+            if (genericExceptions.contains(exceptionTypeString)) {
             	this.throwsGenericCount += 1;
                 System.out.println("[ANTIPATTERN WARNING] 'Throws Generic' anti-pattern detected: " + getLocation(node.getStartPosition()));
                 System.out.println("----");
